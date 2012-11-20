16265a3370d7a9a16be9cff69f178391e802d619
James Muranga <jmured@gmail.com>
2012-11-20 14:47:06 +0300

    refactored code

--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -1,23 +1,32 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+
 public class CalculatorTest {
 
-    @Test
-    public void shouldAddTwoNumbers(){
-        Calculator calculator = new Calculator();
-        assertThat(calculator.add(5,5),is(10));
+    private Calculator calculator;
+
+    @Before
+    public void setupCalc() {
+        calculator = new Calculator();
     }
 
     @Test
-    public void shouldSubtractTwoNumbers(){
-       Calculator calculator = new Calculator();
-       assertThat(calculator.subtract(4,2), is(2));
+    public void shouldAddTwoNumbers() {
+        assertThat(calculator.add(5, 5), is(10));
     }
+
     @Test
-    public void shouldMultiplyTwoNumbers(){
-        Calculator calculator = new Calculator();
-        assertThat(calculator.multiply(2,3),is(6));
+    public void shouldSubtractTwoNumbers() {
+        assertThat(calculator.subtract(4, 2), is(2));
     }
+
+    @Test
+    public void shouldMultiplyTwoNumbers() {
+        assertThat(calculator.multiply(2, 3), is(6));
+    }
+
+
 }
